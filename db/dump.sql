46ec7210b5472054493718ac6a6392e04f070518
Ryan Christian Rayo <m.ryanchristianrayo@gmail.com>
2023-10-17 14:04:13 +0800

    db Update

--- db/dump.sql
+++ db/dump.sql
@@ -16,6 +16,60 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `days`
+--
+
+DROP TABLE IF EXISTS `days`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `days` (
+  `day_id` int(11) NOT NULL AUTO_INCREMENT,
+  `day_name` varchar(10) NOT NULL,
+  PRIMARY KEY (`day_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `days`
+--
+
+LOCK TABLES `days` WRITE;
+/*!40000 ALTER TABLE `days` DISABLE KEYS */;
+INSERT INTO `days` VALUES (1,'Monday'),(2,'Tuesday'),(3,'Wednesday'),(4,'Thursday'),(5,'Friday'),(6,'Saturday'),(7,'Sunday');
+/*!40000 ALTER TABLE `days` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `room_availability`
+--
+
+DROP TABLE IF EXISTS `room_availability`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `room_availability` (
+  `room_id` varchar(10) NOT NULL,
+  `day_id` int(11) NOT NULL,
+  `slot_id` int(11) NOT NULL,
+  `available` int(11) NOT NULL DEFAULT 1,
+  KEY `room_id` (`room_id`),
+  KEY `day_id` (`day_id`),
+  KEY `slot_id` (`slot_id`),
+  CONSTRAINT `room_availability_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`room_id`),
+  CONSTRAINT `room_availability_ibfk_2` FOREIGN KEY (`day_id`) REFERENCES `days` (`day_id`),
+  CONSTRAINT `room_availability_ibfk_3` FOREIGN KEY (`slot_id`) REFERENCES `time_slots` (`slot_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `room_availability`
+--
+
+LOCK TABLES `room_availability` WRITE;
+/*!40000 ALTER TABLE `room_availability` DISABLE KEYS */;
+/*!40000 ALTER TABLE `room_availability` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `rooms`
 --
 
@@ -24,8 +78,8 @@ DROP TABLE IF EXISTS `rooms`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `rooms` (
   `room_id` varchar(10) NOT NULL,
-  `room_type` varchar(10) NOT NULL,
-  `capacity` int(11) NOT NULL,
+  `room_type` varchar(20) NOT NULL,
+  `capacity` int(5) NOT NULL,
   PRIMARY KEY (`room_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -36,8 +90,33 @@ CREATE TABLE `rooms` (
 
 LOCK TABLES `rooms` WRITE;
 /*!40000 ALTER TABLE `rooms` DISABLE KEYS */;
-INSERT INTO `rooms` VALUES ('Con101','LEC',50);
+INSERT INTO `rooms` VALUES ('CON101','LEC',40),('CON102','LEC',50),('CON103','COM-LAB',60),('CON104','SCI-LAB',45),('ORC14','LEC',40);
 /*!40000 ALTER TABLE `rooms` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `time_slots`
+--
+
+DROP TABLE IF EXISTS `time_slots`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `time_slots` (
+  `slot_id` int(11) NOT NULL AUTO_INCREMENT,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL,
+  PRIMARY KEY (`slot_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `time_slots`
+--
+
+LOCK TABLES `time_slots` WRITE;
+/*!40000 ALTER TABLE `time_slots` DISABLE KEYS */;
+INSERT INTO `time_slots` VALUES (1,'07:30:00','09:00:00'),(2,'09:00:00','10:30:00'),(3,'10:30:00','12:00:00'),(4,'01:00:00','02:30:00'),(5,'02:30:00','04:00:00'),(6,'04:00:00','05:30:00'),(7,'05:30:00','07:00:00');
+/*!40000 ALTER TABLE `time_slots` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -49,4 +128,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-14  7:25:51
+-- Dump completed on 2023-10-17 14:02:58
